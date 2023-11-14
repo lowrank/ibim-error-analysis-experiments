20097b2a8fa47c8e2f8e0299ad4048005c357128
Yimin Zhong <yzz0225@auburn.edu>
2023-11-14 14:35:15 -0600

    fix typo

--- test_line.m
+++ test_line.m
@@ -4,16 +4,16 @@ opt.random_rot = true;
 opt.acc = 0.25;
 
 % integrand function
-opt.f =  @(x, y)(3*x^2).* (x > -0.5) .* (x < 0.5); % test integrand 
+opt.f =  @(x, y)(3*(15*x+x^2)).* (x > -0.5) .* (x < 0.5); % test integrand 
 % regularity
-opt.q = 1;
+opt.q = 2;
 
 % tube width
 opt.alpha= 1;
-opt.beta = (3 - alpha)/2; % variance of random slope
+opt.beta = (3 - opt.alpha)/2; % variance of random slope
 opt.upsilon = 0.5;
 
-K = 50; % number of grid sizes.
+K = 24; % number of grid sizes.
 S = 32;  % number of sampled translations.
 
 ibim_2d_experiments(K, S, opt)
